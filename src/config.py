410940ffbf4236fa02340f8b6ddfce7e2b4f1777
Ubuntu <ubuntu@ip-172-31-17-70.ap-southeast-2.compute.internal>
2021-03-09 09:45:19 +0000

    nginx

--- src/config.py
+++ src/config.py
@@ -2,5 +2,5 @@ import os
 
 S3_BUCKET = os.environ.get("S3_BUCKET")
 S3_KEY = os.environ.get("S3_KEY")
-S3_SECRET = os.environ.get("S3_SECRET")
+S3_SECRET = os.environ.get("S3_SECRET_ACCESS_KEY")
 S3_URL = os.environ.get("S3_URL")
